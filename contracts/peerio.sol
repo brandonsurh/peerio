26b06f0be02ae669756d53fd2507d56d09f4972c
brandonsurh <brandonsurh@gmail.com>
2022-11-18 21:23:18 -0500

    Created structs, refactored functions.

--- contracts/peerio.sol
+++ contracts/peerio.sol
@@ -10,7 +10,9 @@
 // - voting on article functionality
 // - fraction of subscribed people is required peer review votes needed
 // - only one paper per person to be peer reviewed
-// 
+// - data struct to hold papers to be reviewed (strings?, count?, 
+// - have someone push to that struct with paper to be reviewed
+// - struct for paper data (name, id (hashed from string?), votes (total and separate), states (voting, passed, failed))
 
 // Methods for smart contract:
 // subscribe/pay funds to smart contract
@@ -27,6 +29,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
 contract peerio {
 
     // amount of money in contract
@@ -35,29 +38,77 @@ contract peerio {
     // mapping for how much each address has paid
     mapping(address => uint) public payments;
     mapping(address => bool) public voted;
+    mapping(address => bool) public hasSubmittedPaper;
+
+    mapping(address => User) public users;
 
     // owner of the contract (probably needs to change)
     address payable public owner;
+
+    // put these in paper struct
     uint public upvotes;
     uint public downvotes;
+
+    // maybe put this in user struct/mapping
     uint public rounds;
+
+    uint id;
 
     //contract settings
     constructor() {
         owner = payable(msg.sender); // setting the contract creator
     }
 
+    // STRUCTS
+    struct User {
+        uint userPayments;
+        uint reputationScore;
+        uint numberOfRounds;
+        bool activeMember;
+        bool voted;         // !!!!!!people wont vote only once!!!!!!!
+        bool hasSubmittedArticle;
+        bool registered;
+        //mapping (uint => Article) public article;
+    }
+
+    struct Article {
+        string title;
+        uint articleId;
+        uint upvotes;
+        uint downvotes;
+        address uploader;
+        //ArticleStatus status;
+        //mapping (uint => Article) public article;
+    }
+
+    mapping(uint256 => Article) articles;
+    uint256[] public articleList;
+
     // allows users to send money to contract
     function subscribe() public payable {
+        if (users[msg.sender].registered == false)
+            registerUser();
         totalBalance += msg.value;
-        payments[msg.sender] += msg.value;
+        users[msg.sender].userPayments += msg.value;
+        users[msg.sender].activeMember = true;
     }
     
     // returns true if an address has paid
-    function userSubscribed(address _addr) public view returns (bool) {
-        if (payments[_addr] > 0)
+    function isUserSubscribed(address _addr) public view returns (bool) {
+        if (users[_addr].activeMember)
             return true;
         return false;
+    }
+
+
+    function registerUser() internal {
+        User storage newUser = users[msg.sender];
+        newUser.reputationScore = 1/2 * 10;
+        newUser.registered = true;
+    }
+
+    function showMePerson() public view returns (uint) {
+        return users[msg.sender].userPayments;
     }
 
     // public function to return total balance
@@ -65,20 +116,52 @@ contract peerio {
         return totalBalance;
     }
 
-    function makeUpvote() public {
-        require(userSubscribed(msg.sender), "You haven't subscribed!");
-        require(voted[msg.sender] == false, "You already voted!");
-        upvotes++;
-        rounds++;
-        voted[msg.sender] = true;
+    // this arg takes in the id of the article that is being voted on
+    function makeUpvote(uint articleId) public {
+        require(isUserSubscribed(msg.sender), "You haven't subscribed!");
+        require(users[msg.sender].voted == false, "You already voted!");
+        // need to change vote lines
+        articles[articleId].upvotes++;
+        users[msg.sender].numberOfRounds++;
+        users[msg.sender].voted = true;
     }
 
-    function makeDownvote() public {
-        require(userSubscribed(msg.sender), "You haven't subscribed!");
-        require(voted[msg.sender] == false, "You already voted!");
-        downvotes++;
-        rounds++;
-        voted[msg.sender] = true;
+    // this arg takes in the id of the article that is being voted on
+    function makeDownvote(uint articleId) public {
+        require(isUserSubscribed(msg.sender), "You haven't subscribed!");
+        require(users[msg.sender].voted == false, "You already voted!");
+        articles[articleId].downvotes++;
+        users[msg.sender].numberOfRounds++;
+        users[msg.sender].voted = true;
     }
+
+    // fill arguments with paper struct vars
+    function proposeReview(string memory title) public {
+        // fill struct with passed args
+        // push paper to data structure of papers to be reviewed
+        // set mapping hasSubmittedArticle to true
+        require(users[msg.sender].hasSubmittedArticle == false, "Your article is awaiting approval!");
+        Article storage newArticle = articles[id];
+        newArticle.articleId = id;
+        newArticle.title = title;
+        //newArticle.upvotes = 0;
+        //newArticle.downvotes = 0;
+        newArticle.uploader = msg.sender;
+        users[msg.sender].hasSubmittedArticle = true;
+        articleList.push(id);
+        id++;
+        // push it to the stack of articles in review
+    }
+
+    // should take in paper struct as args
+    function isVotingDone() public view returns (bool) {
+        // are there enough votes?
+        // needs to be at least 25 percent of peer reviewers vote
+        // maybe also set results in paper struct
+        // if failed review then discard paper from data structure
+        // set hasSubmittedArticle false for uploader
+    }
+
+
 
 }
