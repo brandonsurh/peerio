db716e17b87b9dd0e447f1c773ca6d6694d2a447
Thomas <tombutler4@hotmail.com>
2022-11-20 01:55:42 -0600

    tableland connecting to goerli

--- src/tableland_db.js
+++ src/tableland_db.js
@@ -2,7 +2,8 @@
 import { connect } from "@tableland/sdk";
 
 export default async function CreateArticleTable() {
-    const tableland = connect({ chain: "local-tableland" });
+    const tableland = await connect({ network: "testnet", chain: "ethereum-goerli"});
+    await tableland.siwe();
     const { article_table } = await tableland.create(
       `id integer primary key, title text, description text, author text, cid text`, // Table schema definition
     );
