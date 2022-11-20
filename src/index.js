6458ac32816bf794704d00bfd92de73f453e00ac
electrone901 <carbajalluis@rocketmail.com>
2022-11-19 21:39:48 -0500

    merge changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from "./pages/HomePage"
-import UploadPage from "./pages/UploadPage"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/upload" element={<UploadPage />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
