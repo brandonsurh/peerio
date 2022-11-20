06047da32ad030c5f19c73e672f07603cf38d194
electrone901 <carbajalluis@rocketmail.com>
2022-11-19 22:02:14 -0500

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import HomePage from './pages/HomePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/App.css'
 import Header from './components/Header'
 import UploadPage from './pages/UploadPage'
+var ethers = require('ethers')
 
 const App = () => {
+  const [wallet, setWallet] = useState('')
+
+  const connectWallet = async () => {
+    let connection = new Promise(async (resolve, reject) => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+      await provider.send('eth_requestAccounts', [])
+      const signer = provider.getSigner()
+      resolve(signer.getAddress())
+      console.log('Account: ', await signer.getAddress())
+    })
+    let result = await connection
+    setWallet(result)
+  }
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header connectWallet={connectWallet} wallet={wallet} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />}></Route>
