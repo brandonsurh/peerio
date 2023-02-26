9c23260c94955ea0c567896db1c79cc78a34e754
Thomas <tombutler4@hotmail.com>
2023-02-25 20:53:49 -0600

    firebase db integration

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,23 +1,30 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
-require("webpack-dotenv").config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
-  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`,
-  measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+  apiKey: "AIzaSyCDhKoI78m2-QhnnTtjJHjWb9HyUwqNgsU",
+  authDomain: "peerio-9ad88.firebaseapp.com",
+  databaseURL: "https://peerio-9ad88-default-rtdb.firebaseio.com",
+  projectId: "peerio-9ad88",
+  storageBucket: "peerio-9ad88.appspot.com",
+  messagingSenderId: "477143120299",
+  appId: "1:477143120299:web:5dbb55c96556a85fc1faee",
+  measurementId: "G-RP2SDT5G3Y"
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export { analytics, database };
